0864fd3fb23ae3203ad045436e31deae6a770330
Nick Schradick <nick.schradick@innogames.com>
2019-09-20 16:48:30 +0200

    Initial commit

---
+++ src/test/java/com/innogames/tdd_example/TddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innogames.tdd_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class TddExampleApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
