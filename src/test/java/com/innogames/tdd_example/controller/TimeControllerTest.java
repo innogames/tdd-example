378481bfcfc2c40c6c7a40054870bbe627f8d9a5
Nick Schradick <nick.schradick@innogames.com>
2019-11-15 15:49:39 +0100

    Add content type header expectation to getTime() test

--- src/test/java/com/innogames/tdd_example/controller/TimeControllerTest.java
+++ src/test/java/com/innogames/tdd_example/controller/TimeControllerTest.java
@@ -1,27 +1,31 @@
 package com.innogames.tdd_example.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class TimeControllerTest {
+
   @Autowired
   private MockMvc mockMvc;
 
   @Test
   void getTime_success() throws Exception {
     final ResultActions resultActions = mockMvc.perform(
-        MockMvcRequestBuilders
-            .get("/api/time")
+        get("/api/time")
     );
 
-    resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+    resultActions
+        .andExpect(status().isOk())
+        .andExpect(header().string("Content-Type", "application/json"));
   }
 }
