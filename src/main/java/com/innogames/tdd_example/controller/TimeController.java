b7c44a2238e9e235d8fc762c1068369fc2fea3b4
Nick Schradick <nick.schradick@innogames.com>
2019-11-15 15:15:00 +0100

    Accept request to /api/time and return 200 OK status

--- src/main/java/com/innogames/tdd_example/controller/TimeController.java
+++ src/main/java/com/innogames/tdd_example/controller/TimeController.java
@@ -1,8 +1,20 @@
 package com.innogames.tdd_example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TimeController {
+
+  @RequestMapping(
+      method = RequestMethod.GET,
+      path = "/api/time"
+  )
+  ResponseEntity getTime() {
+    return new ResponseEntity(HttpStatus.OK);
+  }
 
 }
