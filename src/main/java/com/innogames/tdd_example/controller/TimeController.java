326d58c2abfd12a23c0fde6da2258d06bade1622
Nick Schradick <nick.schradick@innogames.com>
2019-11-18 19:42:29 +0100

    Make tests for getTime V1.0.0 pass.

--- src/main/java/com/innogames/tdd_example/controller/TimeController.java
+++ src/main/java/com/innogames/tdd_example/controller/TimeController.java
@@ -7,19 +7,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TimeController {
 
+  private static final String API_VERSION_HEADER = "X-API-VERSION";
+
   @RequestMapping(
       method = RequestMethod.GET,
       path = "/api/time",
       produces = MediaType.APPLICATION_JSON_VALUE
   )
-  ResponseEntity<TimeResponse> getTime() {
-    HttpHeaders responseHeaders = new HttpHeaders();
+  ResponseEntity<TimeResponse> getTime(
+      @RequestHeader(name = API_VERSION_HEADER) final String apiVersion
+  ) {
+    final HttpHeaders responseHeaders = new HttpHeaders();
+    responseHeaders.add(API_VERSION_HEADER, apiVersion);
     responseHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
     final TimeResponse timeResponse = new TimeResponse(LocalDateTime.now());
     return new ResponseEntity<>(timeResponse, responseHeaders, HttpStatus.OK);
